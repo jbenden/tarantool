8f82d3150985b14ee5b97e6c254eaba4d74051f4
Vladimir Davydov <vdavydov.dev@gmail.com>
2017-06-19 18:40:06 +0300

    vinyl: add per-index cache statistics
    This patch adds the cache section to index.info with the following
    counters in it:
    
      cache
        rows                # number of tuples in the cache
        bytes               # cache memory size
        lookup              # lookups in the cache
        get                 # reads from the cache
          rows
          bytes
        put                 # write to the cache
          rows
          bytes
        invalidate          # overwrites in the cache
          rows
          bytes
        evict               # evictions due to memory quota
          rows
          bytes
    
    Needed for #1662

--- src/box/vy_cache.c
+++ src/box/vy_cache.c
@@ -90,12 +90,14 @@ vy_cache_entry_new(struct vy_cache_env *env, struct vy_cache *cache,
 	rlist_add(&env->cache_lru, &entry->in_lru);
 	env->mem_used += vy_cache_entry_size(entry);
 	env->cached_count++;
+	vy_stmt_counter_acct_tuple(&cache->stat.count, stmt);
 	return entry;
 }
 
 static void
 vy_cache_entry_delete(struct vy_cache_env *env, struct vy_cache_entry *entry)
 {
+	vy_stmt_counter_unacct_tuple(&entry->cache->stat.count, entry->stmt);
 	assert(env->cached_count > 0);
 	env->cached_count--;
 	assert(env->mem_used >= vy_cache_entry_size(entry));
@@ -188,6 +190,7 @@ vy_cache_gc_step(struct vy_cache_env *env)
 		}
 	}
 	cache->version++;
+	vy_stmt_counter_acct_tuple(&cache->stat.evict, entry->stmt);
 	vy_cache_tree_delete(&cache->cache_tree, entry);
 	vy_cache_entry_delete(cache->env, entry);
 }
@@ -293,6 +296,8 @@ vy_cache_add(struct vy_cache *cache, struct tuple *stmt,
 		entry->left_boundary_level = boundary_level;
 	else if (direction < 0 && boundary_level < entry->right_boundary_level)
 		entry->right_boundary_level = boundary_level;
+
+	vy_stmt_counter_acct_tuple(&cache->stat.put, stmt);
 
 	/* Done if it's not a chain */
 	if (prev_stmt == NULL)
@@ -413,6 +418,8 @@ vy_cache_on_write(struct vy_cache *cache, const struct tuple *stmt)
 	if (exact) {
 		cache->version++;
 		struct vy_cache_entry *to_delete = *entry;
+		vy_stmt_counter_acct_tuple(&cache->stat.invalidate,
+					   to_delete->stmt);
 		vy_cache_tree_delete(&cache->cache_tree, to_delete);
 		vy_cache_entry_delete(cache->env, to_delete);
 	}
@@ -489,6 +496,14 @@ vy_cache_iterator_is_end_stop(struct vy_cache_iterator *itr,
 	}
 }
 
+static void
+vy_cache_iterator_get(struct vy_cache_iterator *itr, struct tuple **ret)
+{
+	*ret = itr->curr_stmt;
+	if (*ret != NULL)
+		vy_stmt_counter_acct_tuple(&itr->cache->stat.get, *ret);
+}
+
 /**
  * Find next (lower, older) record with the same key as current
  *
@@ -499,13 +514,14 @@ vy_cache_iterator_start(struct vy_cache_iterator *itr, struct tuple **ret,
 {
 	assert(!itr->search_started);
 	assert(itr->curr_stmt == NULL);
-	itr->stat->lookup_count++;
 	*ret = NULL;
 	*stop = false;
 	itr->search_started = true;
 	itr->version = itr->cache->version;
 	struct vy_cache_tree *tree = &itr->cache->cache_tree;
 	const struct tuple *key = itr->key;
+
+	itr->cache->stat.lookup++;
 
 	if (tuple_field_count(itr->key) > 0) {
 		bool exact;
@@ -552,8 +568,7 @@ vy_cache_iterator_start(struct vy_cache_iterator *itr, struct tuple **ret,
 	}
 	itr->curr_stmt = candidate;
 	tuple_ref(itr->curr_stmt);
-	*ret = itr->curr_stmt;
-	return;
+	vy_cache_iterator_get(itr, ret);
 }
 
 static void
@@ -609,7 +624,6 @@ vy_cache_iterator_next_key(struct vy_stmt_iterator *vitr,
 	}
 	if (!itr->curr_stmt) /* End of search. */
 		return 0;
-	itr->stat->step_count++;
 
 	struct vy_cache_tree *tree = &itr->cache->cache_tree;
 	int dir = iterator_direction(itr->iterator_type);
@@ -671,7 +685,7 @@ vy_cache_iterator_next_key(struct vy_stmt_iterator *vitr,
 		itr->curr_stmt = stmt;
 		tuple_ref(itr->curr_stmt);
 	}
-	*ret = itr->curr_stmt;
+	vy_cache_iterator_get(itr, ret);
 	return 0;
 }
 
@@ -728,11 +742,11 @@ vy_cache_iterator_restore(struct vy_stmt_iterator *vitr,
 				vy_cache_tree_iterator_get_elem(tree, &pos);
 			assert(entry != NULL);
 			vy_cache_iterator_is_stop(itr, *entry, stop);
-			*ret = itr->curr_stmt;
+			vy_cache_iterator_get(itr, ret);
 			return 0;
 		}
 		if (itr->version == itr->cache->version) {
-			*ret = itr->curr_stmt;
+			vy_cache_iterator_get(itr, ret);
 			return 0;
 		}
 		while (true) {
@@ -759,7 +773,7 @@ vy_cache_iterator_restore(struct vy_stmt_iterator *vitr,
 				vy_cache_iterator_is_stop(itr, *entry, stop);
 			}
 		}
-		*ret = itr->curr_stmt;
+		vy_cache_iterator_get(itr, ret);
 		return rc;
 	}
 
@@ -820,13 +834,11 @@ static struct vy_stmt_iterator_iface vy_cache_iterator_iface = {
 };
 
 void
-vy_cache_iterator_open(struct vy_cache_iterator *itr,
-		       struct vy_iterator_stat *stat, struct vy_cache *cache,
+vy_cache_iterator_open(struct vy_cache_iterator *itr, struct vy_cache *cache,
 		       enum iterator_type iterator_type,
 		       const struct tuple *key, const struct vy_read_view **rv)
 {
 	itr->base.iface = &vy_cache_iterator_iface;
-	itr->stat = stat;
 
 	itr->cache = cache;
 	itr->iterator_type = iterator_type;
